61dfb2f7859cb58ae7644edb177dfa4e73939713
Fredrik Anborén <fredrik.anboren.syne22lin@edu.tucsweden.se>
2023-11-30 10:35:22 +0100

    Datatypes done

--- InstapotAPI/Entity/Image.cs
+++ InstapotAPI/Entity/Image.cs
@@ -2,7 +2,13 @@
 namespace InstapotAPI.Entity;
 public class Image
 {
+    public int Id { get; set; }
     public string Path { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public int UserID { get; set; }
+    public List<Comment> Comments { get; set; }
+    public bool isPublished { get; set; }
+    public List<int> LikedBy { get; set; }
 }
