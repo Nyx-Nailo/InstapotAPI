e15c9016933a6b18ce8326254e3e0f49fdd48b69
Fredrik Anborén <fredrik.anboren.syne22lin@edu.tucsweden.se>
2023-12-05 14:09:32 +0100

    1

--- InstapotAPI.Tests/InfrastructureTests/RepositoriesTests/ImageRepositoryTests.cs
+++ InstapotAPI.Tests/InfrastructureTests/RepositoriesTests/ImageRepositoryTests.cs
@@ -1,13 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using InstapotAPI.Infrastructure.Repositories;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 
 namespace InstapotAPI.Tests.InfrastructureTests.RepositoriesTests;
 [TestClass]
 public class ImageRepositoryTests
 {
+    private DbContext _dbContext;
+    private ImageRepository _sut;
+    [TestInitialize]
+    public void Initializer()
+    {
+        var connection = new SqliteConnection("DataSource=:memory:");
+        connection.Open();
+    }
+
     [TestMethod]
     public void test()
     {
