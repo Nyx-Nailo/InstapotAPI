08e4e3b4a5487bbe72e94a76d5a0c48e0665ccb6
Fredrik Anborén <fredrik.anboren.syne22lin@edu.tucsweden.se>
2023-12-05 14:44:52 +0100

    1

--- InstapotAPI.Tests/InfrastructureTests/RepositoriesTests/ImageRepositoryTests.cs
+++ InstapotAPI.Tests/InfrastructureTests/RepositoriesTests/ImageRepositoryTests.cs
@@ -1,4 +1,6 @@
 ﻿
+using InstapotAPI.Entity;
+using InstapotAPI.Infrastructure;
 using InstapotAPI.Infrastructure.Repositories;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
@@ -9,16 +11,45 @@ public class ImageRepositoryTests
 {
     private DbContext _dbContext;
     private ImageRepository _sut;
+    private Image[] _testImages;
     [TestInitialize]
     public void Initializer()
     {
+        _testImages = [
+            new Image { UserID = 1, Path = "this should be a img path", Title = "Image1", Description = "Desc of Image1", CreatedDate = DateTime.Now, Comments = new List<Comment>(), isPublished = true, LikedBy = new List<int>() },
+            new Image { UserID = 1, Path = "this should be a img path", Title = "Image2", Description = "Desc of Image2", CreatedDate = DateTime.Now, Comments = new List<Comment>(), isPublished = true, LikedBy = new List<int>() },
+            new Image { UserID = 1, Path = "this should be a img path", Title = "Image3", Description = "Desc of Image3", CreatedDate = DateTime.Now, Comments = new List<Comment>(), isPublished = true, LikedBy = new List<int>() },
+            new Image { UserID = 1, Path = "this should be a img path", Title = "Image4", Description = "Desc of Image4", CreatedDate = DateTime.Now, Comments = new List<Comment>(), isPublished = true, LikedBy = new List<int>() },
+            new Image { UserID = 1, Path = "this should be a img path", Title = "Image5", Description = "Desc of Image5", CreatedDate = DateTime.Now, Comments = new List<Comment>(), isPublished = true, LikedBy = new List<int>() },
+            new Image { UserID = 1, Path = "this should be a img path", Title = "Image6", Description = "Desc of Image6", CreatedDate = DateTime.Now, Comments = new List<Comment>(), isPublished = true, LikedBy = new List<int>() },
+            new Image { UserID = 1, Path = "this should be a img path", Title = "Image7", Description = "Desc of Image7", CreatedDate = DateTime.Now, Comments = new List<Comment>(), isPublished = true, LikedBy = new List<int>() },
+            new Image { UserID = 1, Path = "this should be a img path", Title = "Image8", Description = "Desc of Image8", CreatedDate = DateTime.Now, Comments = new List<Comment>(), isPublished = true, LikedBy = new List<int>() },
+            new Image { UserID = 1, Path = "this should be a img path", Title = "Image9", Description = "Desc of Image9", CreatedDate = DateTime.Now, Comments = new List<Comment>(), isPublished = true, LikedBy = new List<int>() },
+        ];
         var connection = new SqliteConnection("DataSource=:memory:");
         connection.Open();
-    }
 
+        _dbContext = new InstapotContext(new DbContextOptionsBuilder<InstapotContext>().UseSqlite(connection).Options);
+        _dbContext.Database.EnsureCreated();
+
+        _sut = new ImageRepository(_dbContext);
+    }
+    #region CreateNewImage
     [TestMethod]
-    public void test()
+    public void WhenCreatingNewImageReturnTrue()
     {
+        var result = _sut.CreateNewImage(1,"this is a path","title","description",DateTime.Now);
 
+        Assert.IsTrue(result);
     }
+    [TestMethod]
+    public void WhenCreatingNewImageNewImageIsAddedToDB()
+    {
+        var newImage = new Image { UserID = 2, Path = "Path", Title = "NewImage", Description = "Description", CreatedDate = DateTime.Now, isPublished = true, Comments = new List<Comment>(), LikedBy = new List<int>() };
+
+        _sut.CreateNewImage(newImage.UserID, newImage.Path, newImage.Title, newImage.Description, newImage.CreatedDate);
+
+        Assert.AreEqual(newImage, _dbContext.Find(10));
+    }
+    #endregion
 }
--- InstapotAPI/Infrastructure/Repositories/ImageRepository.cs
+++ InstapotAPI/Infrastructure/Repositories/ImageRepository.cs
@@ -1,6 +1,18 @@
-﻿namespace InstapotAPI.Infrastructure.Repositories;
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace InstapotAPI.Infrastructure.Repositories;
 
 public class ImageRepository
 {
+    DbContext _dbContext;
+    public ImageRepository(DbContext context)
+    {
+        _dbContext = context;
+    }
+
+    public bool CreateNewImage(int userID, string path, string title, string description, DateTime createdDate)
+    {
+        return true;
+    }
 
 }
